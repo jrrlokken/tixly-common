2a144cf27c62b96dcb8caec4c2f0a895fc157c99
Joshua Lokken <joshualokken@pm.me>
2021-11-13 15:11:49 -0600

    Updates

--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -4,6 +4,8 @@ export enum Subjects {
 
   OrderCreated = "order:created",
   OrderCancelled = "order:cancelled",
+
+  ExpirationComplete = "expiration:complete",
 }
 
 const printSubject = (subject: Subjects) => {};
